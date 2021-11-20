64f74e54201d2ee2bb5c9980d09c2b3fc2d1437f
Bruno Leonardo Sancho Ventura <86660522+brunoventura123@users.noreply.github.com>
2021-11-20 16:33:14 -0300

    Mudança no calculo

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var price = document.querySelector('#productPrice');
 var tValue = document.querySelector('#totalValue');
 var aReceived = document.querySelector('#amountReceived');
 var productValue = '';
-var total = '';
+var total = 0;
 var thing = '';
 document.body.addEventListener('keydown', function (event) {
     var code = event.keyCode;
@@ -11,7 +11,7 @@ document.body.addEventListener('keydown', function (event) {
         productValue = price.value;
         showAll.innerHTML += "R$ " + Number(productValue).toFixed(2) + "<br/>";
         var soma = Number(total) + Number(productValue);
-        total = soma.toString();
+        total = soma;
         tValue.innerHTML = "R$ " + Number(total).toFixed(2);
         price.value = '';
     }
@@ -22,7 +22,8 @@ function totalToPay() {
         thing = conta.toString();
         showAll.innerHTML = showAll.innerHTML + " <hr/>Total: " + tValue.innerHTML + "\n            <br/> Valor recebido: R$ " + Number(aReceived.value).toFixed(2) + " <br/>\n            <p style=\"color: green\">Troco: R$ " + Number(thing).toFixed(2) + "</p><br/>";
         Number(total);
-        if (aReceived.value < total) {
+        Number(aReceived.value);
+        if (Number(aReceived.value) < total) {
             showAll.innerHTML += '<p style="color:red;">Valor recebido é menor que o valor Total!</p><br/>';
         }
     }
@@ -35,5 +36,5 @@ function clean() {
     tValue.innerHTML = '';
     productValue = '';
     thing = '';
-    total = '';
+    total = 0;
 }
--- script.ts
+++ script.ts
@@ -5,7 +5,7 @@ let tValue = document.querySelector('#totalValue') as HTMLDivElement
 let aReceived = document.querySelector('#amountReceived') as HTMLInputElement
 
 let productValue = ''
-let total = ''
+let total = 0
 let thing = ''
 document.body.addEventListener('keydown', function(event){
     const code = event.keyCode;
@@ -13,7 +13,7 @@ document.body.addEventListener('keydown', function(event){
         productValue = price.value
         showAll.innerHTML += `R$ ${Number(productValue).toFixed(2)}<br/>`
         let soma = Number(total) + Number(productValue)
-        total = soma.toString()
+        total = soma
         tValue.innerHTML = `R$ ${Number(total).toFixed(2)}` 
         price.value = ''
 }
@@ -28,7 +28,7 @@ function totalToPay(){
             <p style="color: green">Troco: R$ ${Number(thing).toFixed(2)}</p><br/>`
             Number(total)
             Number(aReceived.value)
-            if(aReceived.value < total){
+            if(Number(aReceived.value) < total){
                 showAll.innerHTML += '<p style="color:red;">Valor recebido é menor que o valor Total!</p><br/>'
             }
         }
@@ -42,5 +42,5 @@ function clean(){
     tValue.innerHTML = ''
     productValue = ''
     thing = ''
-    total = ''
+    total = 0
 }
